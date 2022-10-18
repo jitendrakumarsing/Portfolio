51642339282d0436b8fd2ff76e857f21ce3a74d4
jitendra kumar <jk540499@gmail.com>
2022-10-18 19:42:20 +0530

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,4 @@
+import { click } from '@testing-library/user-event/dist/click';
 import React from 'react'
 import { useState,useEffect} from 'react';
 import '../css/navbar.css'
@@ -31,17 +32,16 @@ let list = document.getElementById("list-items");
 list.style.display = list.style.display !== 'grid' ? 'grid' : 'none';
 
 }
-
   return (
    <>
    <nav className="navbar">
     <span className="logo">Jitendra Kumar</span>
     
     <ul id='list-items' className={smallscreen?'smalldevices':'nav-links'}>
-    <li><a className="link" href="/#Home"  >Home</a></li>
-    <li><a  className="link" href="#Work">Work</a></li>
-    <li><a className="link" href="#Education">Education</a></li>
-    <li><a className="link" href="#About">About</a></li>
+    <li><a className="link" href="/#Home" onClick={handleclick}  >Home</a></li>
+    <li><a  className="link" href="#Work" onClick={handleclick}>Work</a></li>
+    <li><a className="link" href="#Education" onClick={handleclick}>Education</a></li>
+    <li><a className="link" href="#About" onClick={handleclick}>About</a></li>
     </ul>
     <span className='iconbtn' style={smallscreen ? { display:'block'} : {display : 'none'} }>
          <i className="fa-solid fa-bars" onClick={handleclick} ></i></span>
