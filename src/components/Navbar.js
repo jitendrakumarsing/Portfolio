055817117bd8a71b7bf92579ad0ffea96a422f7e
jitendra kumar <jk540499@gmail.com>
2022-10-23 12:09:59 +0530

    nav-update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,3 @@
-import { click } from '@testing-library/user-event/dist/click';
 import React from 'react'
 import { useState,useEffect} from 'react';
 import '../css/navbar.css'
@@ -29,19 +28,19 @@ function handleclick() {
  document.querySelector(".iconbtn>i").classList.toggle("fa-xmark");
 
 let list = document.getElementById("list-items");
-list.style.display = list.style.display !== 'grid' ? 'grid' : 'none';
-
+list.style.display =list.style.display !== 'grid'? 'grid' : 'none';
 }
+let nochange=()=>{}
   return (
    <>
    <nav className="navbar">
     <span className="logo">Jitendra Kumar</span>
     
-    <ul id='list-items' className={smallscreen?'smalldevices':'nav-links'}>
-    <li><a className="link" href="/#Home" onClick={handleclick}  >Home</a></li>
-    <li><a  className="link" href="#Work" onClick={handleclick}>Work</a></li>
-    <li><a className="link" href="#Education" onClick={handleclick}>Education</a></li>
-    <li><a className="link" href="#About" onClick={handleclick}>About</a></li>
+    <ul id='list-items' className={smallscreen?'smalldevices':'nav-links'} onClick={smallscreen?handleclick:nochange}>
+    <li><a className="link" href="/#Home">Home</a></li>
+    <li><a  className="link" href="#Work">Work</a></li>
+    <li><a className="link" href="#Education">Education</a></li>
+    <li><a className="link" href="#About">About</a></li>
     </ul>
     <span className='iconbtn' style={smallscreen ? { display:'block'} : {display : 'none'} }>
          <i className="fa-solid fa-bars" onClick={handleclick} ></i></span>
